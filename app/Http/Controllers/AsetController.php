0935cac076173342241422b5216c0474ce11f6ec
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-11 19:12:26 +0700

    add model kategoris and jenis cmr

--- app/Http/Controllers/AsetController.php
+++ app/Http/Controllers/AsetController.php
@@ -122,7 +122,7 @@ class AsetController extends Controller
     {
         //
         $this->validate($request,[
-            'id' => 'required',
+            //'id' => 'required',
             'nama' => 'required',
     		'deskripsi' => 'required',
             'merek_id' => 'required',
