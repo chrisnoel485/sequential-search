12e65bc15dc5711ce41d8c1ab8dcaa2b5568d446
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-15 20:47:31 +0700

    add posisi history

--- resources/views/posisi/index.blade.php
+++ resources/views/posisi/index.blade.php
@@ -1,16 +1,16 @@
 @extends('adminlte::page')
 
-@section('title', 'Daftar Aset')
+@section('title', 'Histori Posisi Aset')
 
 @section('content_header')
     <div class="row mb-2">
         <div class="col-sm-6">                      
-            <h1 class="m-0 text-dark">Manajemen Aset</h1>
+            <h1 class="m-0 text-dark">Manajemen Histori Posisi Aset</h1>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                <li class="breadcrumb-item active">Aset</li>
+                <li class="breadcrumb-item active">Posisi</li>
             </ol>
         </div>
     </div>
@@ -22,10 +22,10 @@
             <div class="card">
                 <div class="card-header">
                     <div>
-                        <h3 class="card-title">Data Aset</h3>
+                        <h3 class="card-title">Data Histori Posisi Aset</h3>
                     </div>
                     <div class="card-tools">
-                        <a href="/aset/create" class="btn btn-primary">Input Aset Baru</a>
+                        <a href="/aset/create" class="btn btn-primary">Histori Posisi Aset</a>
                     </div>
                 </div>
                 <div class="card-body">
@@ -37,7 +37,6 @@
                                         <th class="text-center">No</th>
                                         <th class="text-center">Nama Aset</th>
                                         <th class="text-center">Letak</th>
-                                        <th class="text-center">Status</th>
                                         <th class="text-center">Tindakan</th>
                                     </tr>
                                 </thead>
