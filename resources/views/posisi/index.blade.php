4e7977b253801e7830d8cf610089b6f970f0087f
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-16 09:46:15 +0700

    add posisi history

--- resources/views/posisi/index.blade.php
+++ resources/views/posisi/index.blade.php
@@ -1,16 +1,16 @@
 @extends('adminlte::page')
 
-@section('title', 'Histori Posisi Aset')
+@section('title', 'Daftar Aset')
 
 @section('content_header')
     <div class="row mb-2">
         <div class="col-sm-6">                      
-            <h1 class="m-0 text-dark">Manajemen Histori Posisi Aset</h1>
+            <h1 class="m-0 text-dark">Manajemen Aset</h1>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                <li class="breadcrumb-item active">Posisi</li>
+                <li class="breadcrumb-item active">Aset</li>
             </ol>
         </div>
     </div>
@@ -22,10 +22,10 @@
             <div class="card">
                 <div class="card-header">
                     <div>
-                        <h3 class="card-title">Data Histori Posisi Aset</h3>
+                        <h3 class="card-title">Data Aset</h3>
                     </div>
                     <div class="card-tools">
-                        <a href="/aset/create" class="btn btn-primary">Histori Posisi Aset</a>
+                        <a href="/aset/create" class="btn btn-primary">Input Aset Baru</a>
                     </div>
                 </div>
                 <div class="card-body">
@@ -36,24 +36,48 @@
                                     <tr>
                                         <th class="text-center">No</th>
                                         <th class="text-center">Nama Aset</th>
+                                        <th class="text-center">Deskripsi</th>
+                                        <th class="text-center">Merek</th>
+                                        <th class="text-center">Kategori</th>
+                                        <th class="text-center">Jenis</th>
                                         <th class="text-center">Letak</th>
+                                        <th class="text-center">Status</th>
                                         <th class="text-center">Tindakan</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-						            @foreach($aset as $a)
-						                <tr>
-							                <td>{{ $a->nama }}</td>
-							                <td>
-								                <ul>
-									                @foreach($a->letak as $h)
-									                    <li> {{ $h->nama}} </li>
-									                @endforeach
-								                </ul>
-							                </td>
-						                </tr>
-						            @endforeach
-					            </tbody>
+                                    @php $no = 1; @endphp
+                                    @forelse($aset as $p)
+                                    <tr>
+                                        <td class="text-center">{{ $no++ }}</td>
+                                        <td class="text-center">{{ $p->nama }}</td>
+                                        <td class="text-center" cols="5" rows="5">{{ $p->deskripsi }}</td>
+                                        <td class="text-center">{{ $p->merek->nama }}</td>
+                                        <td class="text-center">{{ $p->kategori->nama }}</td>
+                                        <td class="text-center">{{ $p->jenis->nama }}</td>
+                                        <td>
+                                            @foreach($p->letak as $h)
+								                <li> {{ $h->nama}} </li>
+									        @endforeach
+                                        </td>
+                                        <td class="text-center">{{ $p->status }}</td>
+                                        <td class="text-center">
+                                            <form action="{{ route('aset.destroy', $p->id) }}" method="POST">
+                                                @csrf
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                    <div class="btn-group">
+                                                        <a class="btn btn-info" href="{{ route('aset.show', $p->id) }}"><i class="fa fa-eye"></i></a>
+                                                        <a class="btn btn-success" href="{{ route('aset.edit', $p->id) }}"><i class="fa fa-edit"></i></a>
+                                                        <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                                    </div>
+                                            </form>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <td colspan="8" class="text-center">Tidak ada data</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
                             </table>
                         </div>
                     </div>
