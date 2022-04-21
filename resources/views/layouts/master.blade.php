279a4f832fdc81c421e7b03128cc18ad59e8e994
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-21 19:18:12 +0700

    add role

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,8 +20,8 @@
     <div class="wrapper">
         <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+            <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="index3.html" class="nav-link">Home</a>
