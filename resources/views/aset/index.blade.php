03bf4b2a1a7501bc5041aff598aa21dd635f6d79
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-21 22:14:33 +0700

    add role

--- resources/views/aset/index.blade.php
+++ resources/views/aset/index.blade.php
@@ -1,22 +1,27 @@
-@extends('adminlte::page')
-
-@section('title', 'Daftar Aset')
-
-@section('content_header')
-    <div class="row mb-2">
-        <div class="col-sm-6">                      
-            <h1 class="m-0 text-dark">Manajemen Aset</h1>
-        </div>
-        <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                <li class="breadcrumb-item active">Aset</li>
-            </ol>
-        </div>
-    </div>
-@stop
+@extends('layouts.master')
+@section('title')
+    <title>Daftar Aset</title>
+@endsection
 
 @section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">                      
+                        <h1 class="m-0 text-dark">Manajemen Aset</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                            <li class="breadcrumb-item active">Aset</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="content">
+            <div class="container-fluid">
 <div class="row">
         <div class="col-12">
             <div class="card">
@@ -77,5 +82,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    </div>
+        </section>
     </div>
 @stop
--- resources/views/posisi/index.blade.php
+++ resources/views/posisi/index.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.master')
-
-@section('title', 'Daftar Aset')
-
-@section('content_header')
-    <div class="row mb-2">
-        <div class="col-sm-6">                      
-            <h1 class="m-0 text-dark">Manajemen Aset</h1>
-        </div>
-        <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                <li class="breadcrumb-item active">Aset</li>
-            </ol>
-        </div>
-    </div>
-@stop
+@section('title')
+    <title>Daftar Posisi Aset</title>
+@endsection
 
 @section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">                      
+                        <h1 class="m-0 text-dark">Manajemen Posisi Aset</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                            <li class="breadcrumb-item active">Posisi Aset</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="content">
+            <div class="container-fluid">
 <div class="row">
         <div class="col-12">
             <div class="card">
@@ -80,5 +85,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    </div>
+        </section>
     </div>
 @stop
