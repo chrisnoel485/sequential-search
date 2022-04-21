30bed85675b9cc171fa6cb8e3489c08b3a0d6b39
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-21 21:38:39 +0700

    add role

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.master')
-
-@section('title', 'Tambah Kategori')
-
-@section('content_header')
-    <div class="row mb-2">
-        <div class="col-sm-6">                      
-            <h1 class="m-0 text-dark">Manajemen Kategori</h1>
-        </div>
-        <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                <li class="breadcrumb-item active">Kategori</li>
-            </ol>
-        </div>
-    </div>
-@stop
+@section('title')
+    <title>Tambah Kategori</title>
+@endsection
 
 @section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">                      
+                        <h1 class="m-0 text-dark">Manajemen Kategori</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        ol class="breadcrumb float-sm-right">
+                         <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                        <li class="breadcrumb-item active">Kategori</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
     <div class="row">
         <div class="col-12">
             <div class="card">
