5d8d7422ce70cc4885676689ac0eebb1141b5eca
Chris Noel Sunda Dirgantara <chris.dirgantara@sequis.com>
2022-04-30 10:39:45 +0700

    add role

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use App\Http\Controllers\MerekController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect(route('login'));
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
